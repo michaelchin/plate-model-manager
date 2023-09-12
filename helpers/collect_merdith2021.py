7722aed73c7cab3da3e33d43bbb197d185425af3
michaelchin <michaelchin78@hotmail.com>
2023-09-12 12:12:13 +1000

    update collect_merdith2021.py

--- helpers/collect_merdith2021.py
+++ helpers/collect_merdith2021.py
@@ -1,29 +1,36 @@
-import sys
+import sys, requests, zipfile, io, glob
 
 import utils
 
 
 model_path = utils.get_model_path(sys.argv, "merdith2021")
 
-# fetch coastlines
-utils.fetch_coastlines(
-    "https://www.earthbyte.org/webdav/ftp/incoming/mchin/plate-models/MERDITH2021/shapes_coastlines_Merdith_et_al_v2.gpmlz",
-    model_path,
-    "shapes_coastlines_Merdith_et_al_v2.gpmlz",
+r = requests.get(
+    "https://earthbyte.org/webdav/ftp/Data_Collections/Merdith_etal_2021_ESR/SM2-Merdith_et_al_1_Ga_reconstruction_v1.1.zip",
+    allow_redirects=True,
 )
+if r.status_code in [200]:
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(f"{model_path}/")
+
+# zip topologies
+files = glob.glob(f"{model_path}/**/[!shapes_]*.gpml")
+utils.zip_files(files, f"{model_path}/Topologies.zip", "Topologies")
+
+# zip coastlines
+files = glob.glob(f"{model_path}/**/shapes_coastlines*")
+utils.zip_files(files, f"{model_path}/Coastlines.zip", "Coastlines")
 
 
-# fetch static polygons
-utils.fetch_static_polygons(
-    "https://www.earthbyte.org/webdav/ftp/incoming/mchin/plate-models/MERDITH2021/shapes_static_polygons_Merdith_et_al.gpmlz",
-    model_path,
-    "shapes_static_polygons_Merdith_et_al.gpmlz",
-)
+# zip static polygons
+files = glob.glob(f"{model_path}/**/shapes_static_polygons*")
+utils.zip_files(files, f"{model_path}/StaticPolygons.zip", "StaticPolygons")
 
 
-# fetch rotations
-utils.fetch_rotations(
-    "https://www.earthbyte.org/webdav/ftp/incoming/mchin/plate-models/MERDITH2021/1000_0_rotfile_Merdith_et_al_optimised.rot",
-    model_path,
-    "1000_0_rotfile_Merdith_et_al_optimised.rot",
-)
+# zip rotations
+files = glob.glob(f"{model_path}/**/*.rot")
+utils.zip_files(files, f"{model_path}/Rotations.zip", "Rotations")
+
+# zip ContinentalPolygons
+files = glob.glob(f"{model_path}/**/shapes_continents*")
+utils.zip_files(files, f"{model_path}/ContinentalPolygons.zip", "ContinentalPolygons")
