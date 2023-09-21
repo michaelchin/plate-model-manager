4476789482896daf39198633aecfbfa213696344
michaelchin <michaelchin78@hotmail.com>
2023-09-21 20:13:31 +1000

    update test_plate_model.py

--- unittest/test_plate_model.py
+++ unittest/test_plate_model.py
@@ -1,47 +1,106 @@
 #!/usr/bin/env python
 import os
-import sys
+import sys, logging, unittest
+from pathlib import Path
 
 sys.path.insert(0, f"{os.path.dirname(__file__)}/../src")
 from common import TEMP_TEST_DIR
 
-from plate_model_manager import PlateModel, PlateModelManager
+from plate_model_manager import PlateModelManager
+
+if __name__ == "__main__":
+    logger_name = "test_plate_model_main"
+else:
+    logger_name = __name__
+
+logger = logging.getLogger(logger_name)
+Path("logs").mkdir(parents=True, exist_ok=True)
+fh = logging.FileHandler(f"logs/{logger_name}.log")
+fh.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s \n\n%(message)s\n")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.setLevel(logging.INFO)
 
 
-def main():
-    model_manager = PlateModelManager(f"{os.path.dirname(__file__)}/../models.json")
+class PlateModelTestCase(unittest.TestCase):
+    def setUp(self):
+        model_manager = PlateModelManager(f"{os.path.dirname(__file__)}/../models.json")
 
-    # test remote models.json with URL
-    # model_manager = plate_model.PlateModelManager(
-    #    "https://www.earthbyte.org/webdav/ftp/gplately/models.json"
-    # )
+        # test remote models.json with URL
+        # model_manager = plate_model.PlateModelManager(
+        #    "https://www.earthbyte.org/webdav/ftp/gplately/models.json"
+        # )
+        self.model_name = "Muller2019"
+        self.model = model_manager.get_model(self.model_name)
+        self.model.set_data_dir(TEMP_TEST_DIR)
 
-    model = model_manager.get_model("Muller2019")
-    model.set_data_dir(TEMP_TEST_DIR)
+    def test_get(self):
+        logger.info("test_get ...")
 
-    print(model.get_avail_layers())
+        layers = self.model.get_avail_layers()
+        self.assertTrue(len(layers) > 0)
+        logger.info(layers)
 
-    print(model.get_rotation_model())
+        rotation_files = self.model.get_rotation_model()
+        self.assertTrue(len(rotation_files) > 0)
+        logger.info(rotation_files)
+        for f in rotation_files:
+            self.assertTrue(os.path.isfile(f))
 
-    print(model.get_layer("Coastlines"))
+        coastlines_files = self.model.get_layer("Coastlines")
+        self.assertTrue(len(coastlines_files) > 0)
+        logger.info(coastlines_files)
+        for f in coastlines_files:
+            self.assertTrue(os.path.isfile(f))
 
-    print(model.get_COBs())
+        cob_files = self.model.get_COBs()
+        self.assertTrue(len(cob_files) > 0)
+        logger.info(cob_files)
+        for f in cob_files:
+            self.assertTrue(os.path.isfile(f))
 
-    print(model.get_topologies())
+        topology_files = self.model.get_topologies()
+        self.assertTrue(len(topology_files) > 0)
+        logger.info(topology_files)
+        for f in topology_files:
+            self.assertTrue(os.path.isfile(f))
 
-    model.download_all_layers()
+        data_dir = self.model.get_data_dir()
+        self.assertEqual(TEMP_TEST_DIR, data_dir)
+        logger.info(data_dir)
+        self.assertTrue(os.path.isdir(data_dir))
 
-    model.download_time_dependent_rasters("AgeGrids", times=[1, 2])
+        filepath = self.model.get_raster("AgeGrids", 10)
+        self.assertTrue(len(filepath) > 0)
+        logger.info(filepath)
+        self.assertTrue(os.path.isfile(filepath))
 
-    print(model.get_data_dir())
+        filepaths = self.model.get_rasters("AgeGrids", [10, 11, 12, 13, 14])
+        self.assertEqual(len(filepaths), 5)
+        logger.info(filepaths)
+        for f in filepaths:
+            self.assertTrue(os.path.isfile(f))
 
-    print(model.get_raster("AgeGrids", 10))
+    def test_download(self):
+        logger.info("test_download ...")
 
-    print(model.get_rasters("AgeGrids", [10, 11, 12, 13, 14]))
+        self.model.download_all_layers()
 
-    # this will download a large volume of data
-    # model.download_all()
+        self.model.download_time_dependent_rasters("AgeGrids", times=[1, 2])
+
+    @unittest.skip("this will download a large volume of data")
+    def test_download_all(self):
+        self.model.download_all()
 
 
 if __name__ == "__main__":
-    main()
+    # use the following code to run a list of tests
+    # suite = unittest.TestSuite()
+    # suite.addTest(PlateModelTestCase("test_get"))
+    # runner = unittest.TextTestRunner()
+    # runner.run(suite)
+
+    # use the following code to run all tests in this file
+    runner = unittest.TextTestRunner()
+    runner.run(unittest.makeSuite(PlateModelTestCase))
