6287187b230a31cd99d756f59615862d438d5739
michaelchin <michaelchin78@hotmail.com>
2023-10-11 19:20:59 +1100

    Update build-doc.sh

--- scripts/build-doc.sh
+++ scripts/build-doc.sh
@@ -6,6 +6,7 @@ pip-compile pyproject.toml
 pip3 install .
 rm doc/source/plate_model_manager.rst
 rm doc/source/modules.rst
+pip3 install -U sphinx sphinx_rtd_theme
 sphinx-apidoc -o doc/source src/plate_model_manager/
 cd doc
 make html
