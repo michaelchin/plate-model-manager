fc0ab4ab99611c533eade02b238d5f2a359c1dbe
michaelchin <michaelchin78@hotmail.com>
2023-11-14 20:10:28 +1100

    "update version"

--- src/plate_model_manager/__init__.py
+++ src/plate_model_manager/__init__.py
@@ -1,7 +1,7 @@
 # import functions into __init__.py
 
-__version__ = "1.0.4"
+__version__ = "1.1.2"
 
-from .plate_model_manager import PlateModelManager
 from .plate_model import PlateModel
+from .plate_model_manager import PlateModelManager
 from .present_day_rasters import PresentDayRasterManager
