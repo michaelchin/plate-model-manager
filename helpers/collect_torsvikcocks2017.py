c8601d9ab8fb2cba790bdd100b37e785db526018
michaelchin <michaelchin78@hotmail.com>
2024-03-20 20:48:40 +1100

    update torsvikcocks2017 helper script

--- helpers/collect_torsvikcocks2017.py
+++ helpers/collect_torsvikcocks2017.py
@@ -1,34 +1,45 @@
-import os
+import glob
+import io
+import shutil
 import sys
+import zipfile
+from datetime import datetime
 
+import requests
 import utils
 
 model_path = utils.get_model_path(sys.argv, "torsvikcocks2017")
+zip_path = "CEED6"
 
-# fetch Rotations
-utils.fetch_and_zip_files(
-    [
-        "https://www.earthbyte.org/webdav/ftp/incoming/mchin/plate-models/TorsvikCocks2017/Torsvik_Cocks_HybridRotationFile.rot"
-    ],
-    model_path,
-    "Rotations",
-)
+info_fp = open(f"{model_path}/info.txt", "w+")
+info_fp.write(f"{datetime.now()}\n")
+
+# download the model zip file
+zip_url = "https://www.earthdynamics.org/earthhistory/bookdata/CEED6.zip"
+info_fp.write(f"Download zip file from {zip_url}\n")
+r = requests.get(zip_url, allow_redirects=True, verify=True)
+if r.status_code in [200]:
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(f"{model_path}/{zip_path}")
 
 
-# fetch Coastlines
-utils.fetch_and_zip_files(
-    [
-        "https://www.earthbyte.org/webdav/ftp/incoming/mchin/plate-models/TorsvikCocks2017/Torsvik_Cocks_2016_Terranes.gpmlz",
-    ],
-    model_path,
-    "Coastlines",
-)
+# zip Rotations
+files = glob.glob(f"{model_path}/{zip_path}/*.rot")
+utils.zip_files(files, f"{model_path}/Rotations.zip", "Rotations", info_fp)
 
-# fetch StaticPolygons
-utils.fetch_and_zip_files(
-    [
-        "https://www.earthbyte.org/webdav/ftp/incoming/mchin/plate-models/TorsvikCocks2017/Torsvik_Cocks_2016_Terranes.gpmlz"
-    ],
-    model_path,
-    "StaticPolygons",
-)
+
+# zip StaticPolygons
+files = glob.glob(f"{model_path}/{zip_path}/CEED6_LAND.*")
+files += glob.glob(f"{model_path}/{zip_path}/CEED6_MICROCONTINENTS.*")
+utils.zip_files(files, f"{model_path}/StaticPolygons.zip", "StaticPolygons", info_fp)
+
+
+# zip Coastlines
+files = glob.glob(f"{model_path}/{zip_path}/CEED6_LAND.*")
+files += glob.glob(f"{model_path}/{zip_path}/CEED6_MICROCONTINENTS.*")
+utils.zip_files(files, f"{model_path}/Coastlines.zip", "Coastlines", info_fp)
+
+
+shutil.rmtree(f"{model_path}/{zip_path}")
+
+info_fp.close()
