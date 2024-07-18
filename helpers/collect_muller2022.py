a7ba7d67cf48dcd859d6c637a78d947d2ec9acaa
michaelchin <michaelchin78@hotmail.com>
2024-07-18 12:33:21 +1000

    update muller2022

--- helpers/collect_muller2022.py
+++ helpers/collect_muller2022.py
@@ -9,15 +9,31 @@ from datetime import datetime
 import requests
 import utils
 
+from plate_model_manager.zenodo import ZenodoRecord
+
+# https://zenodo.org/doi/10.5281/zenodo.10297173
+record = ZenodoRecord(10297173)
+latest_id = record.get_latest_version_id()
+print(f"The latest version ID is: {latest_id}.")
+filenames = record.get_filenames(latest_id)
+print(f"The file names in the latest version: {filenames}")
+idx = 0
+for i in range(len(filenames)):
+    if filenames[i].startswith("Muller_etal_2022_SE_1Ga_Opt_PlateMotionModel"):
+        idx = i
+        break
+file_links = record.get_file_links(latest_id)
+print(f"The file links in the latest version: {file_links}")
+
 model_path = utils.get_model_path(sys.argv, "muller2022")
-zip_path = "Muller_etal_2022_SE_1Ga_Opt_PlateMotionModel_v1.2.2"
+zip_path = "Muller_etal_2022_SE_1Ga_Opt_PlateMotionModel"
 
 info_fp = open(f"{model_path}/info.txt", "w+")
 info_fp.write(f"{datetime.now()}\n")
 
 
 # download the model zip file
-zip_url = "https://earthbyte.org/webdav/ftp/Data_Collections/Muller_etal_2022_SE/Muller_etal_2022_SE_1Ga_Opt_PlateMotionModel_v1.2.2"
+zip_url = file_links[idx]
 info_fp.write(f"Download zip file from {zip_url}\n")
 r = requests.get(
     zip_url,
@@ -25,8 +41,7 @@ r = requests.get(
 )
 if r.status_code in [200]:
     z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall(f"{model_path}/")
-
+    z.extractall(f"{model_path}/{zip_path}")
 
 # zip Rotations
 with zipfile.ZipFile(
@@ -35,7 +50,7 @@ with zipfile.ZipFile(
     compression=zipfile.ZIP_DEFLATED,
     compresslevel=9,
 ) as f_zip:
-    f = f"{model_path}/{zip_path}/optimisation/1000_0_rotfile_Merdith_et_al_optimised.rot"
+    f = f"{model_path}/{zip_path}/Optimisation/1000_0_rotfile_Merdith_et_al_optimised.rot"
     info_fp.write(f"Zip Rotations:\n")
     info_fp.write(f"\t{f}\n")
     f_zip.write(f, f"Rotations/{os.path.basename(f)}")
@@ -47,7 +62,7 @@ with zipfile.ZipFile(
     compression=zipfile.ZIP_DEFLATED,
     compresslevel=9,
 ) as f_zip:
-    f = f"{model_path}/{zip_path}/shapes_static_polygons_Merdith_et_al.gpml"
+    f = f"{model_path}/{zip_path}/Static_Polygons/static_polygons_Merdith_etal.gpml"
     info_fp.write(f"Zip StaticPolygons:\n")
     info_fp.write(f"\t{f}\n")
     f_zip.write(f, f"StaticPolygons/{os.path.basename(f)}")
@@ -59,7 +74,7 @@ with zipfile.ZipFile(
     compression=zipfile.ZIP_DEFLATED,
     compresslevel=9,
 ) as f_zip:
-    files = glob.glob(f"{model_path}/{zip_path}/shapes_coastlines_Merdith_et_al.gpmlz")
+    files = glob.glob(f"{model_path}/{zip_path}/Coastlines/coastlines.gpmlz")
     info_fp.write(f"Zip Coastlines:\n")
     for f in files:
         f_zip.write(f, f"Coastlines/{os.path.basename(f)}")
@@ -73,13 +88,13 @@ with zipfile.ZipFile(
     compresslevel=9,
 ) as f_zip:
     files = [
-        f"{model_path}/{zip_path}/1000-410-Convergence.gpml",
-        f"{model_path}/{zip_path}/1000-410-Divergence.gpml",
-        f"{model_path}/{zip_path}/1000-410-Topologies.gpml",
-        f"{model_path}/{zip_path}/250-0_plate_boundaries.gpml",
-        f"{model_path}/{zip_path}/410-250_plate_boundaries.gpml",
-        f"{model_path}/{zip_path}/TopologyBuildingBlocks.gpml",
-        f"{model_path}/{zip_path}/1000-410-Transforms.gpml",
+        f"{model_path}/{zip_path}/Topologies/1000-410-Convergence.gpml",
+        f"{model_path}/{zip_path}/Topologies/1000-410-Divergence.gpml",
+        f"{model_path}/{zip_path}/Topologies/1000-410-Topologies.gpml",
+        f"{model_path}/{zip_path}/Topologies/250-0_plate_boundaries.gpml",
+        f"{model_path}/{zip_path}/Topologies/410-250_plate_boundaries.gpml",
+        f"{model_path}/{zip_path}/Topologies/TopologyBuildingBlocks.gpml",
+        f"{model_path}/{zip_path}/Topologies/1000-410-Transforms.gpml",
     ]
     info_fp.write(f"Zip Topologies:\n")
     for f in files:
@@ -93,7 +108,7 @@ with zipfile.ZipFile(
     compression=zipfile.ZIP_DEFLATED,
     compresslevel=9,
 ) as f_zip:
-    f = f"{model_path}/{zip_path}/COB_polygons_and_coastlines_combined_1000_0_Merdith_etal.gpml"
+    f = f"{model_path}/{zip_path}/COB_Coasts/COB_polygons_and_coastlines_combined_1000_0_Merdith_etal.gpml"
     f_zip.write(
         f,
         f"COBs/{os.path.basename(f)}",
@@ -108,7 +123,7 @@ with zipfile.ZipFile(
     compression=zipfile.ZIP_DEFLATED,
     compresslevel=9,
 ) as f_zip:
-    files = glob.glob(f"{model_path}/{zip_path}/shapes_continents.gpml")
+    files = glob.glob(f"{model_path}/{zip_path}/Continents/continents.gpml")
     info_fp.write(f"Zip ContinentalPolygons:\n")
     for f in files:
         f_zip.write(f, f"ContinentalPolygons/{os.path.basename(f)}")
@@ -121,7 +136,7 @@ with zipfile.ZipFile(
     compression=zipfile.ZIP_DEFLATED,
     compresslevel=9,
 ) as f_zip:
-    files = glob.glob(f"{model_path}/{zip_path}/shapes_cratons_Merdith_et_al.gpml")
+    files = glob.glob(f"{model_path}/{zip_path}/Cratons/cratons.gpml")
     info_fp.write(f"Zip Cratons:\n")
     for f in files:
         f_zip.write(f, f"Cratons/{os.path.basename(f)}")
@@ -129,6 +144,5 @@ with zipfile.ZipFile(
 
 
 shutil.rmtree(f"{model_path}/{zip_path}")
-shutil.rmtree(f"{model_path}/__MACOSX")
 
 info_fp.close()
